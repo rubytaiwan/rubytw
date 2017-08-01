c46c34f04d0d48bbcbfc13440f74d0bfcc461fac
Irene Chang <irene84111@gmail.com>
2017-08-01 17:52:01 +0800

    updated Communities section on the front page

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class CommunitiesController < Fae::BaseController
 
+    def index
+      @items = Community.order('id')
+    end
+
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def landing
     @events = Event.order('start_date DESC')
+    @communities = Community.all
   end
 end
