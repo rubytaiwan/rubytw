afc5941e5c10117de797b5be59bc03ff425d0482
Irene Chang <irene84111@gmail.com>
2017-08-25 11:45:11 +0800

    set SEO

---
+++ app/helpers/seo_helper.rb
@@ -0,0 +1,30 @@
+module SeoHelper
+  def seo_meta_tags
+    [meta_tags(@seo[:meta]), og_meta_tags(@seo[:og]), google_meta_tags(@seo[:google]), twitter_meta_tags(@seo[:twitter])].delete_if { |v| v.blank? }.inject(&:+) if @seo
+  end
+
+  private
+
+  # http://ogp.me/
+  def og_meta_tags **hash
+    meta_tags(hash){|name, value| tag(:meta, property: "og:#{name}", content: value) }
+  end
+
+  # http://schema.org/
+  def google_meta_tags **hash
+    hash.delete :item_type
+    meta_tags(hash){|name, value| tag(:meta, itemprop: name, content: value) }
+  end
+
+  def twitter_meta_tags hash = {}
+    hash = hash.presence || @seo_twitter
+    meta_tags(hash){|name, value| tag(:meta, name: "twitter:#{name}", content: value) }
+  end
+
+  def meta_tags **hash, &block
+    tags = hash.delete_if { |k, v| v.blank? }.map{ |name, value|
+      value = image_url(value) if name == :image
+      block_given? ? yield(name, value) : tag(:meta, name: name, content: value)
+    }.inject(&:+) if hash.respond_to?(:map)
+  end
+end
