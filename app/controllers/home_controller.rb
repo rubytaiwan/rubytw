7d1355efd6edce047f4c8ff3e422619934864e18
蒼時弦也 <elct9620@frost.tw>
2017-09-14 16:16:33 +0800

    Add permalink feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def landing
-    @blogs = Blog.where(active: true).order('date_of_news DESC')
-    @events = Event.order('start_on DESC')
+    @blogs = Blog.where(active: true).order(date_of_news: :desc).order(created_at: :desc)
+    @events = Event.order(start_on: :desc)
     @events_active = Event.where(active: true)
     @events_ended = Event.where(active: false)
     @communities = Community.all
