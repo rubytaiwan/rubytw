a5ce70ee27c6d9c8648f8f02485624877a29c2c9
蒼時弦也 <elct9620@frost.tw>
2017-09-14 16:10:28 +0800

    Add slug column for blog

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -5,11 +5,15 @@ module Admin
       @items = Blog.order('date_of_news DESC')
     end
 
+    def new
+      super
+      @item.date_of_news = Time.zone.now
+    end
+
     private
 
     def build_assets
       @item.build_cover_image if @item.cover_image.blank?
     end
-
   end
 end
